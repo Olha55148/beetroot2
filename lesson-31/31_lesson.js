438129924fd91160e0f4074db2cb42161ce45958
Olha55148 <Sotnik_24@ukr.net>
2023-04-13 01:58:44 -0700

    First commit

---
+++ lesson-31/31_lesson.js
@@ -0,0 +1,119 @@
+// Min 
+// const age2 = Number(prompt('Скажіть Ваш вік'));
+// підлітком (12-17), дорослим (18_59) або пенсіонером (60 ...),
+
+
+// if (age2 <= 11) {
+//     alert(`Ви є дитиною`);
+// } else if (age2 >= 12 && age2 <= 17) {
+//     alert(`Ви є підлітком`);
+// } else if (age2 >=18 && age2 <= 59) {
+//     alert(`Ви є дорослим`);
+// } else if (age2 >=60) {
+//     alert (`Ви є пенсіонером`)
+// } else {
+//     alert(`Error`)
+// };
+
+
+// let numberUser = Number(prompt('Назвіть число від 0 до 9'));
+// виведи йому спецсимвол, який розташований на цій клавіші (1 !, 2 @, 3 # і т. д).
+// switch (numberUser) {
+//     case 0:
+//         alert(`)`);
+//         break;
+//     case 1:
+//         alert(`!`);
+//         break;
+//     case 2:
+//         alert(`@`);
+//         break;
+//     case 3:
+//         alert(`#`);
+//         break;
+//     case 4:
+//         alert(`$`);
+//         break;
+//     case 5:
+//         alert(`%`);
+//         break;
+//     case 6:
+//         alert(`^`);
+//         break;
+//     case 7:
+//         alert(`&`);
+//         break;
+//     case 8:
+//         alert(`*`);
+//         break;
+//     case 9:
+//         alert(`(`);
+//         break;
+//     default:
+//         alert(`Error`)
+// };
+
+
+// let count = 0;
+// while (count < 5) {
+//     console.log('Som text', count);
+//     count++
+// }
+// do {
+//     console.log('Som text', count);
+//     count++ 
+// } while (count < 5);
+
+// let array = [
+//     40,
+//     "some",
+//     57,
+//     80,
+//     "help",
+//     78,
+//     [2, "key",89],
+//     54,
+//     65
+// ]
+ 
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     if (Array.isArray(element)) {
+//         for (let k = 0; k < element.length; k++) {
+//             console.log(element[k]);
+//         } 
+//     }
+//     else {
+//         console.log(element);
+//     } 
+// };
+
+
+// let count4 = Number(prompt("Назвіть номер"));
+// while (count4 > 0) {
+//     console.log(count4);
+//     count4--
+// };
+// let m = prompt('Введіть число')
+// let lamb = '';
+
+// for (let l = 1; l < m; l++) {
+//     lamb += `${l} вівця...`
+// }
+// console.log(lamb);
+
+let number14 = prompt('Введіть число');
+let number6 = '';
+
+for (let i5 = number14-1 ; i5 > 0; i5--) {
+    
+    number6 = number6 + `# `
+    console.log(number6);
+}
+
+
+
+
+
+ 
+
