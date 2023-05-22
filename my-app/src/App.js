439e17b65b6c7f740b3677c7188415cb08bcba6c
Olha55148 <Sotnik_24@ukr.net>
2023-05-22 16:41:14 -0700

    React

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.scss';
+import {Header} from "./Components/Header/Header"
+
+function App() {
+  let text = 'Some'
+  return (
+    <> 
+    <Header></Header>
+ </>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Header/Header.jsx
@@ -0,0 +1,3 @@
+const Header = () => <header>
+    some header
+</header>
