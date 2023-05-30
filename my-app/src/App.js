4d2ded022ee2f6e9bb4690852b4462c0c23a0685
Olha55148 <Sotnik_24@ukr.net>
2023-05-30 06:51:20 -0700

    lesson45

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,18 @@ import Header1 from './Components/Header1/Header1';
 import Banner from './Components/Banner/Banner';
 import Blog from './Components/Blog/Blog';
 import News from './Components/News/News';
+// const data = [
+//   {
+//   id: 2,
+//   title: "title 2"
+// },
+// {
+//   id: 2,
+//   title: "title 2"
+// },
+// ]
 function App() {
-  let text = 'Some'
+
   return (
     <>
       <div className='wrapper'>
@@ -14,8 +24,10 @@ function App() {
         </Header1>
         <Banner></Banner>
         <main className='main2'>
-          <Blog></Blog>
+         
+          <Blog> </Blog>
           <News></News>
+          {/* {data.map(({title, id}) => <Title key={id} title={title}/>)} */}
         </main>
       </div>
     </>
--- my-app/src/Components/News/News.jsx
+++ my-app/src/Components/News/News.jsx
@@ -1,13 +1,29 @@
 import "./News.scss";
 import News1 from './News1/News1';
-const News = (props) => <div className="div-news">
-    <span className="title-general">News</span>
-    <div className="news1-wrqpper">
-        <News1></News1>
-        <News1></News1>
-        <News1></News1>
-    </div>
+import axios from 'axios'
+import { useEffect, useState } from "react";
+// const News = (props) => <div className="div-news">
+//     <span className="title-general">News</span>
+//     <div className="news1-wrqpper">
+//         <News1></News1>
+//         <News1></News1>
+//         <News1></News1>
+//     </div>
+
+
+// </div>
+
+
+const News = () => {
+    const [newsData, setBlogData] = useState([]);
+    useEffect (() => {
+        axios('https://newsdata.io/api/1/news?apikey=pub_23187b5c37554a3f6d491a7af7f5fe9b5b29a&q=cryptocurrency').then(res => setBlogData(res.data.results))
+    }, []);
+return <div className="div-news"> News
+{newsData.map(item => <News1 key={item.id} {...item}></News1>)}
 
 
 </div>
+};
+
 export default News
